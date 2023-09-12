6600df7052c2dce130ff9c5769852923b29a0dfd
F2RUZ <gaffarovferuz4@gmail.com>
2023-09-12 10:29:08 +0500

    1-dars

---
+++ main.js
@@ -0,0 +1,56 @@
+// let name = "nimadir"; // string
+
+// let age = "20"; // number
+
+// let isStudents = falsee; // boolean
+
+// var isStudents; // underfind
+
+// let age = null; // null
+
+// let name = "Feruz";
+// let age = 20;
+
+// let result = `${name} ${age} yoshda`;
+// console.log(result);
+// let name = "nimadir";
+// let result = name + num2; // matematic usul
+// let result = num2 - num; // matematic ayirish
+// let result = num * num2;
+// let result = num2 / num; // mateamtic bolish
+// let result = num **16   matematic daraga
+// let result = num2 % num;  qoldiqli bolish
+
+// let result = Math.round(num)
+// let num = 20;
+// let num2 = 61;
+
+// console.log(result);
+
+// let name = prompt("iltimos ismingizni kiriting !!!");
+// let age = prompt("Iltimos yoshingizni kiriting !!!");
+// let num = Number(age);
+// let year = 2023 - num;
+
+// alert(`${name} siz ${year} yilda tugulgan ekansiz  `);
+
+// let num = 20;
+
+// console.log(--num);
+// let result = Math.floor(num);
+// let result = Math.ceil(num);
+
+// let num = 4.4;
+
+// console.log(result);
+
+let name = prompt("ismingizni kiriting !!!");
+let score = prompt("olgan natijangizni kiritng !!");
+
+let ball = Number(score);
+
+if (ball > 80) {
+  alert("siz  qabul qilingiz");
+} else {
+  alert("qayta urin");
+}
